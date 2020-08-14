47045f794ad79ceec3b208b75f75885df73b4aca
alfahmi <gst.alfahmi@gmail.com>
2020-08-14 15:56:11 +0800

    " Showing word NumbersActivity.java "

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,37 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Create a list of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+
+        int index = 0;
+        while (index < words.size() ) {
+//            Log.d("MYINT","Value" + i);
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+            index++;
+        }
+
+        for(int indexu = 0; indexu < words.size();indexu++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(indexu));
+            rootView.addView(wordView);
+        }
+
     }
 }
